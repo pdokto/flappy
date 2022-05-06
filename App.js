eb7faa8b579a5bd18b75c471de59decdff035ccd
Patrick Doktorowski <patrick.doktorowski@googlemail.com>
2022-05-06 12:51:27 +0200

    bird rectangle

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import { GameEngine } from 'react-native-game-engine';
+import entities from './entities';
 
 export default function App() {
   return (
     <View style={{flex:1}}>
-      <Text>Open up App.js to start your app!</Text>
+      <GameEngine 
+        entities={entities()}
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0
+        }}>
+
+      </GameEngine>
       <StatusBar style="auto" hidden={true} />
     </View>
   );
